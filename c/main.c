069faded984c51c31479744f7a857870da6e2039
Radu Marin <radumarin232@gmail.com>
2024-10-30 13:05:34 +0200

    Updated synchronisation. Updated Makefile

--- c/main.c
+++ c/main.c
@@ -2,51 +2,56 @@
 
 #include "retrier.h"
 
-int add(int a, int b) {
-    return a + b;
+int add(int a, int b)
+{
+	return a + b;
 }
 
-fn_retry_status add_wrapper(void* args) {
-    struct {
-        int a;
-        int b;
-    } *fn_args;
+fn_retry_status add_wrapper(void *args)
+{
+	struct {
+		int a;
+		int b;
+	} *fn_args;
 
-    fn_args = args;
+	fn_args = args;
 
-    static volatile int cnt = 3;
+	static volatile int cnt = 3;
 
-    if(cnt != 0) {
-        cnt--;
-        return (fn_retry_status) {
-            .err = "not yet",
-            .ok = 0,
-            .ret = NULL,
-            .retry = 1
-        };
-    }
+	if (cnt != 0) {
+		cnt--;
+		return (fn_retry_status)
+		{
+			.err = "not yet",
+				.ok = 0,
+				.ret = NULL,
+				.retry = 1
+		};
+	}
 
-    // use malloc for stable results
-    volatile int res = add(fn_args->a, fn_args->b);
+	// use malloc for stable results
+	volatile int res = add(fn_args->a, fn_args->b);
 
-    return (fn_retry_status) {
-        .err = "OK",
-        .ok = 1,
-        .ret = &res,
-        .retry = 0
-    };
+	return (fn_retry_status)
+	{
+		.err = "OK",
+			.ok = 1,
+			.ret = &res,
+			.retry = 0
+	};
 }
 
-int main(void) {
-    retrier_t retrier = new_basic_retrier("log.txt");
-    // retrier.cfg.log = stdout;
+int main(void)
+{
+	retrier_t retrier = new_basic_retrier("log.txt");
+	// retrier.log = stdout;
 
-    retry_result res = retry_fn(add_wrapper, &(struct { int a;  int b }) {
-        .a = 2, .b = 3
-    }, &retrier);
+	retry_result res = retry_fn(add_wrapper, &(struct { int a;  int b }) {
+		.a = 2, .b = 3
+	}, &retrier);
 
-    int r = *((int*)res.final_status.ret);
-    printf("%s: %d\nRetry cnt: %d\n", res.err, r, res.retry_cnt);
+	int r = *((int *)res.final_status.ret);
+	printf("%s: %d\nRetry cnt: %d\n", res.err, r, res.retry_cnt);
 
-    return 0;
+	return 0;
 }
--- c/retrier.c
+++ c/retrier.c
@@ -2,89 +2,84 @@
 
 #include "retrier.h"
 
-retrier_t new_basic_retrier(const char* log_file) {
-    retrier_t retrier = new_default_retrier();
+retrier_t new_basic_retrier(const char *log_file)
+{
+	retrier_t retrier = new_default_retrier();
+	if (log_file == NULL) {
+		return retrier;
+	}
 
-    retrier.cfg.log = fopen(log_file, "at");
-    if(retrier.cfg.log == NULL) {
-        return retrier;
-    }
-
-    int rc = pthread_mutex_init(&retrier.cfg.mux, NULL);
-    if(rc < 0) {
-        retrier.cfg.log = NULL;
-    }
-
-    return retrier;
+	retrier.log = fopen(log_file, "at");
+	return retrier;
 }
 
-retrier_t new_retrier(const retrier_cfg* cfg) {
-    return (retrier_t) {
-        .cfg = *cfg
-    };
+retrier_t new_default_retrier(void)
+{
+	return (retrier_t)
+	{
+		.cnt = 5,
+			.max_sleep_time = 1000000000,
+			.log = NULL
+	};
 }
 
-retrier_t new_default_retrier(void) {
-    return (retrier_t) {
-        .cfg = (retrier_cfg){
-            .cnt = 5,
-            .max_sleep_time = 1000000000,
-            .log = NULL
-        }
-    };
+int destroy_retrier(retrier_t *retrier)
+{
+	if (retrier->log) {
+		return fclose(retrier->log);
+	}
+	return 0;
 }
 
-int destroy_retrier(retrier_t* retrier) {
-    if(retrier->cfg.log) {
-        fclose(retrier->cfg.log);
-        return pthread_mutex_destroy(&retrier->cfg.mux);
-    }
-    return 0;
-}
+retry_result retry_fn(fn_retry_status(*fn)(void *), void *args, retrier_t *retrier)
+{
+	unsigned int ns = 100;
+	fn_retry_status status;
 
-retry_result retry_fn(fn_retry_status(*fn)(void*), void* args, retrier_t* retrier) {
-    unsigned int ns = 100;
-    fn_retry_status status;
+	for (unsigned int i = 0; i < retrier->cnt; i++) {
+		status = fn(args);
 
-    for(unsigned int i = 0; i < retrier->cfg.cnt; i++) {
-        status = fn(args);
+		if (status.ok) {
+			return (retry_result)
+			{
+				.err = "OK",
+					.final_status = status,
+					.retry_cnt = i + 1
+			};
+		}
 
-        if(status.ok) {
-            return (retry_result) {
-                .err = "OK",
-                    .final_status = status,
-                    .retry_cnt = i + 1
-            };
-        }
+		if (!status.retry) {
+			return (retry_result)
+			{
+				.err = "Retry not recommended by function",
+					.final_status = status,
+					.retry_cnt = i + 1
+			};
+		}
 
-        if(!status.retry) {
-            return (retry_result) {
-                .err = "Retry not recommended by function",
-                    .final_status = status,
-                    .retry_cnt = i + 1
-            };
-        }
+		if (retrier->log != NULL) {
+			// Make it thread-safe
+			flockfile(retrier->log);
+			fprintf(retrier->log, "%s\n", status.err);
+			funlockfile(retrier->log);
+		}
 
-        if(retrier->cfg.log != NULL) {
-            pthread_mutex_lock(&retrier->cfg.mux);
-            fprintf(retrier->cfg.log, "%s\n", status.err);
-            pthread_mutex_unlock(&retrier->cfg.mux);
-        }
+		// Wait before retry
+		nanosleep(&(struct timespec)
+		{
+			.tv_nsec = ns,
+				.tv_sec = 0
+		}, NULL);
 
-        // wait before retry
-        nanosleep(&(struct timespec) {
-            .tv_nsec = ns,
-                .tv_sec = 0
-        }, NULL);
+		if (ns <= (retrier->max_sleep_time >> 1)) {
+			ns <<= 1;
+		}
+	}
 
-        if(ns <= (retrier->cfg.max_sleep_time >> 1)) {
-            ns <<= 1;
-        }
-    }
-
-    return (retry_result) {
-        .err = "Exceeded maximum number of retries",
-            .final_status = status,
-            .retry_cnt = retrier->cfg.cnt
-    };
+	return (retry_result)
+	{
+		.err = "Exceeded maximum number of retries",
+			.final_status = status,
+			.retry_cnt = retrier->cnt
+	};
 }
